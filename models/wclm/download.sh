87ba6cf370a28a21edb1ac6ff07be50528af816a
Shamil Chollampatt <shamilcm@users.noreply.github.com>
2018-06-14 09:42:52 +0800

    Update download.sh

--- models/wclm/download.sh
+++ models/wclm/download.sh
@@ -1,2 +1,2 @@
-curl -L -o wiki.classes.gen.gz https://tinyurl.com/y7j687l3/sparse/wclm/wiki.classes.gen.gz
-curl -L -o wiki.classes.kenlm https://tinyurl.com/y7j687l3/sparse/wclm/wiki.classes.kenlm
+curl -L -o wiki.classes.gen.gz https://tinyurl.com/y7j687l3/wclm/wiki.classes.gen.gz
+curl -L -o wiki.classes.kenlm https://tinyurl.com/y7j687l3/wclm/wiki.classes.kenlm
